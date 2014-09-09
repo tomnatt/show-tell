cd655560832f69e282d6a94b7b76646e7d83646c
tomnatt <github@tomnatt.com>
2014-09-09 13:55:31 +0100

    failed migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140902123401) do
+ActiveRecord::Schema.define(version: 20140909124746) do
 
   create_table "speakers", force: true do |t|
     t.string   "name"
