f5c3a48120eed28d4f2ccbfe9f4fe36452118254
tomnatt <github@tomnatt.com>
2015-05-07 20:43:05 +0100

    added formtastic forms

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -69,6 +69,6 @@ class TalksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def talk_params
-      params[:talk]
+      params.require(:talk).permit(:title, :date, :slides)
     end
 end
