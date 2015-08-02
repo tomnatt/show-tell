9cc92ed03a41eb9a5db7bfee902ca1dc3450c2a6
irisfaraway <iris@iriss-mbp.home>
2015-08-02 20:58:41 +0100

    Turned date strings into datetime

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -11,9 +11,20 @@ doc = Nokogiri::HTML(open("#{target}"))
 
 # Gets dates and spits them out
 doc.xpath('//div[@id="main-content"]//h2').each do |header|
-  h = header.content
+  h = header.content.chomp
 
   # Removes "Agenda for" and stuff in parentheses at the end
-  date = h.sub(/^Agenda\ for\ /, '').sub(/\ *\([a-zA-Z0-9 ]*\)$/, '').chomp
-  puts date
+  datestr = h.sub(/^Agenda\ for\ /, '').sub(/\ *\([a-zA-Z0-9 ]*\)$/, '').chomp
+
+  # Turns the date string into datetime
+  date = DateTime.parse(datestr)
+  puts date.strftime('%v')
 end
+
+# # Splits up each talk - coming back to this later
+
+# doc.xpath('//div[@id="main-content"]//ul/li').each do |item|
+#   x = item.content.chomp
+#   speaker = x.gsub(/\(([a-zA-Z])\)$/, '\1')
+#   puts speaker
+# end
