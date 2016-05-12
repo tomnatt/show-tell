895604e94e0833440c584adcf0f77195f1869a8e
tomnatt <github@tomnatt.com>
2016-05-12 19:42:05 +0100

    deletion of old entries on scrape

--- app/controllers/talk_admin_controller.rb
+++ app/controllers/talk_admin_controller.rb
@@ -1,6 +1,8 @@
 class TalkAdminController < ApplicationController
   def scrape
     # delete all talks
+    Talk.delete_all
+    # generate new
     Scraper.scrape_all_talks
     redirect_to root_path
   end
