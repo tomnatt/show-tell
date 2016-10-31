13cd1d5c71334ae4dc0405597d64c776b8a39911
tomnatt <github@tomnatt.com>
2016-10-31 20:01:51 +0000

    List talk number by years

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'stats#speakers_count'
   get 'stats/speakers'
   get 'stats/speakers_count'
+  get 'stats/:year', to: 'stats#by_year', as: 'by_year'
 
   # admin
   get 'admin/talks/scrape', to: 'talk_admin#scrape'
